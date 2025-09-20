3494fcd745f7788b32c9675d49d1f6cf274f6868
OmKadam-1 <kom110798@gmail.com>
2025-09-21 00:21:12 +0530

    value access

---
+++ javascript/typinggame.js
@@ -0,0 +1,11 @@
+const quotes = [
+  "The quick brown fox jumps over the lazy dog Typing games are fun and useful"
+
+]
+const quoteEl = document.getElementById("quote");
+const inputEl = document.getElementById("input");
+const timeEl = document.getElementById("time");
+const wpmEl = document.getElementById("wpm");
+const startBtn = document.getElementById("startBtn");
+const saveBtn = document.getElementById("saveBtn");
+const scoresEl = document.getElementById("scores");
