8a541d7be6c0cec95f24801da9240a10590c7b18
shreya-harde <shreyaharde29102024@gmail.com>
2025-09-22 02:57:22 -0700

    add some file

---
+++ javascript/login.js
@@ -0,0 +1,52 @@
+<script>
+    
+    function showSignup() {
+      document.getElementById("loginBox").style.display = "none";
+      document.getElementById("signupBox").style.display = "block";
+    }
+
+   
+    function showLogin() {
+      document.getElementById("signupBox").style.display = "none";
+      document.getElementById("loginBox").style.display = "block";
+    }
+
+   
+    document.getElementById("signupForm").addEventListener("submit", function(e) {
+      e.preventDefault();
+      let email = document.getElementById("signupEmail").value;
+      let password = document.getElementById("signupPassword").value;
+
+      if (email === "" || password === "") {
+        alert("Please fill out all fields!");
+        return;
+      }
+
+      let user = {
+        email: email,
+        password: password
+      };
+
+      localStorage.setItem("userData", JSON.stringify(user));
+      alert("Signup successful ✅ Now login!");
+      showLogin();
+    });
+
+    
+    document.getElementById("signupForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let email = document.getElementById("signupEmail").value;
+    let password = document.getElementById("signupPassword").value;
+
+    let userData = {
+        email: email,
+        password: password
+    };
+
+    localStorage.setItem("userData", JSON.stringify(userData));
+    alert("signup successful ✅ Please login now.");
+});
+
+    
+  </script>
