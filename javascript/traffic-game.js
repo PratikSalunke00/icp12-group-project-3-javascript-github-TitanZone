cb83a3ff647d1be5f2fe0ceb79718a66c586d28a
AshishSMathpati <ashishsmathpati@gmail.com>
2025-09-23 21:18:35 +0530

    id given to their respective elements

---
+++ javascript/traffic-game.js
@@ -0,0 +1,6 @@
+let green = false;
+  let car = document.getElementById("car");
+  let car1 = document.getElementById("car1");
+  let car2 = document.getElementById("car2");
+  let redLight = document.getElementById("redLight");
+  let greenLight = document.getElementById("greenLight");
