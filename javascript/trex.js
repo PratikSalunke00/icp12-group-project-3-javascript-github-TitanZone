860ed2eee4ef363ca4747ae64ec70581864237c0
AshishSMathpati <ashishsmathpati@gmail.com>
2025-09-23 23:18:59 +0530

    Declaring variables

---
+++ javascript/trex.js
@@ -0,0 +1,14 @@
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+var cloudsGroup, cloudImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+
+var score;
+
+var gameOverImg,restartImg
+var jumpSound , checkPointSound, dieSound
