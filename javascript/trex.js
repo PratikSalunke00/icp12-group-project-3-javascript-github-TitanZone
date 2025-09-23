ab39a89db0c47a556c182b96503d19e066677f9a
AshishSMathpati <ashishsmathpati@gmail.com>
2025-09-23 23:20:20 +0530

    path added to images

--- javascript/trex.js
+++ javascript/trex.js
@@ -12,3 +12,26 @@ var score;
 
 var gameOverImg,restartImg
 var jumpSound , checkPointSound, dieSound
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  
+  groundImage = loadImage("./../images/ground2.png");
+  
+  cloudImage = loadImage("./../images/cloud.png");
+  
+  obstacle1 = loadImage("./../images/obstacle1.png");
+  obstacle2 = loadImage("./../images/obstacle2.png");
+  obstacle3 = loadImage("./../images/obstacle3.png");
+  obstacle4 = loadImage("./../images/obstacle4.png");
+  obstacle5 = loadImage("./../images/obstacle5.png");
+  obstacle6 = loadImage("./../images/obstacle6.png");
+  
+   restartImg = loadImage("./../images/restart.png")
+  gameOverImg = loadImage("./../images/gameOver.png")
+  
+  jumpSound = loadSound("jump.mp3")
+  dieSound = loadSound("die.mp3")
+  checkPointSound = loadSound("checkPoint.mp3")
+}
