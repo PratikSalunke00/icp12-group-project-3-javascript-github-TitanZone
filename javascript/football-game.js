ffa17b512bcc91e63300da48f25d11d0db65c27d
AshishSMathpati <ashishsmathpati@gmail.com>
2025-09-24 00:18:39 +0530

    js file added for football game

---
+++ javascript/football-game.js
@@ -0,0 +1,10 @@
+let player1, player2, ball, gameover;
+let line1, line4;
+
+function preload(){
+  player1Img = loadImage("player1.png")
+  player2Img = loadImage("player2.png")
+  ballImg = loadImage("ball.png")
+  gameover = loadImage("player1.png")
+
+}
