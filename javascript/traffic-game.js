7ac01c8d18a31d3e7450338d3fcfcb63a3559dc5
AshishSMathpati <ashishsmathpati@gmail.com>
2025-09-23 21:28:30 +0530

    position given to cars

--- javascript/traffic-game.js
+++ javascript/traffic-game.js
@@ -4,3 +4,7 @@ let green = false;
   let car2 = document.getElementById("car2");
   let redLight = document.getElementById("redLight");
   let greenLight = document.getElementById("greenLight");
+
+  let carPos = -100;
+  let car1Pos = 1550;
+  let car2Pos = -600;
