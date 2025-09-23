dc9f68834e1126b759d28d971125a9dc8d274123
RahulNaktode <rahulnaktode04@gmail.com>
2025-09-24 00:00:42 +0530

    add About page

--- javascript/about.js
+++ javascript/about.js
@@ -1,18 +1,49 @@
 const now = new Date();
 const hour = now.getHours();
 let message = "";
-
 if (hour >= 5 && hour < 12) {
-    message = "Good Morning Friend 🌄";
+    message = "Good Morning Gamer 🌄";
 }
 else if (hour >= 12 && hour < 17) {
-    message = "Good Afternoon Friend 🕑";
+    message = "Good Afternoon Gamer 🕑";
 }
 else if (hour >= 17 && hour < 21) {
-    message = "Good Evening Friend 🌆";
+    message = "Good Evening Gamer 🌆";
 }
 else {
-    message = "Good Night Friend 🌃";
+    message = "Good Night Gamer 🌃";
+}
+document.getElementById("greeting").innerText = message;
+
+const images = [
+    { src: "pratikSalunke.jpg", main: "Pratik Salunke", sub: "Founder & Pro-Gamer" },
+    { src: "omKadam.jpg", main: "Om Kadam", sub: "Co-Founder" },
+    { src: "ashish-Mathpati.jpg", main: "Ashish Mathpathi", sub: "Web-Developer & Gamer" },
+    { src: "", main: "", sub: "Web-Developer" },
+    { src: "shreyaharde.jpg", main: "Shreya Harde", sub: "Web-Developer & Reviewer" },
+    { src: "kanchanKadam.jpg", main: "Kanchan Kadam", sub: "Streamer & Reviewer" },
+    { src: "rahul.jpg", main: "Rahul Naktode", sub: "Pro-Gamer & Streamer" }
+];
+
+let index = 0;
+const slideElement = document.getElementById("slide");
+const mainName = document.getElementById("main-name");
+const subName = document.getElementById("sub-name");
+
+function showSlide(i) {
+    slideElement.src = images[i].src;
+    mainName.textContent = images[i].main;
+    subName.textContent = images[i].sub;
 }
 
-document.getElementById("greeting").innerText = message;
+function next() {
+    index = (index + 1) % images.length;
+    showSlide(index);
+}
+
+function prev() {
+    index = (index - 1 + images.length) % images.length;
+    showSlide(index);
+}
+
+setInterval(next, 3000);
