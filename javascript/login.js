50996c7dcc42fd84d450b0c91235d8eb3bc01b9e
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-09-24 09:52:01 +0530

    heeadeer andd ffooter addd

--- javascript/login.js
+++ javascript/login.js
@@ -1,50 +1,54 @@
 
-    function showSignup() {
-      document.getElementById("loginBox").style.display = "none";
-      document.getElementById("signupBox").style.display = "block";
-    }
+function showSignup() {
+  document.getElementById("loginForm").classList.add("hidden");
+  document.getElementById("signupForm").classList.remove("hidden");
+}
 
-   
-    function showLogin() {
-      document.getElementById("signupBox").style.display = "none";
-      document.getElementById("loginBox").style.display = "block";
-    }
 
-   
-    document.getElementById("signupForm").addEventListener("submit", function(e) {
-      e.preventDefault();
-      let email = document.getElementById("signupEmail").value;
-      let password = document.getElementById("signupPassword").value;
+function showLogin() {
+  document.getElementById("signupForm").classList.add("hidden");
+  document.getElementById("loginForm").classList.remove("hidden");
+}
 
-      if (email === "" || password === "") {
-        alert("Please fill out all fields!");
-        return;
-      }
 
-      let user = {
-        email: email,
-        password: password
-      };
+function signup() {
+  let name = document.getElementById("signupName").value;
+  let email = document.getElementById("signupEmail").value;
+  let password = document.getElementById("signupPassword").value;
 
-      localStorage.setItem("userData", JSON.stringify(user));
-      alert("Signup successful ✅ Now login!");
-      showLogin();
-    });
-
-    
-    document.getElementById("signupForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    let email = document.getElementById("signupEmail").value;
-    let password = document.getElementById("signupPassword").value;
-
-    let userData = {
-        email: email,
-        password: password
+  if (name && email && password) {
+    let user = {
+      name: name,
+      email: email,
+      password: password
     };
 
-    localStorage.setItem("userData", JSON.stringify(userData));
-    alert("signup successful ✅ Please login now.");
-});
 
-    
+    localStorage.setItem(email, JSON.stringify(user));
+
+    alert("✅ Signup successful! Now login.");
+
+
+    document.getElementById("signupName").value = "";
+    document.getElementById("signupEmail").value = "";
+    document.getElementById("signupPassword").value = "";
+
+    showLogin();
+  } else {
+    alert("⚠ All fields are required!");
+  }
+}
+
+
+function login() {
+  let email = document.getElementById("loginEmail").value;
+  let password = document.getElementById("loginPassword").value;
+
+  let user = JSON.parse(localStorage.getItem(email));
+
+  if (user && user.password === password) {
+    alert("✅ Login successful! Welcome " + user.name);
+  } else {
+    alert("❌ Invalid Email or Password!");
+  }
+}
