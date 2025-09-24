b9a157ecae4ba0034ed8b7413c9465c721976d2b
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-09-24 12:16:12 +0530

    games added

--- javascript/index.js
+++ javascript/index.js
@@ -1,19 +1,14 @@
 const topgameimgs = [
-  "images/tic-tac-toe.jpg",
-  "images/simon-says.jpg",
-  "images/snake-game.jpg",
-  "images/math-game.jpg",
-  "images/roll-dice.jpg",
-  "images/typing-game.jpg"
+  "images/Simon-Game-img.jpg",
+  "images/snake-game-img.jpg",
+
+  "images/game-2048-img.avif",
 ];
 
 const topgamelinks = [
-  "./pages/tic-tac-toe-game.html",
   "./pages/simon-says.html",
   "./pages/snake-game.html",
-  "./pages/mathpuzzle.html",
-  "./pages/roll-dice-game.html",
-  "./pages/typinggame.html"
+  "./pages/2048-game.html",
 ];
 
 const loadtopgames = () => {
@@ -30,22 +25,23 @@ const loadtopgames = () => {
 loadtopgames();
 
 
+
 const risegameimgs = [
+  "images/traffic-game-img.jpg",
+  "images/math-game.jpg",
+  "images/trex.jpg",
+  "images/dice-roller.jpg",
   "images/tic-tac-toe.jpg",
-  "images/tic-tac-toe.jpg",
-  "images/tic-tac-toe.jpg",
-  "images/tic-tac-toe.jpg",
-  "images/tic-tac-toe.jpg",
-  "images/tic-tac-toe.jpg"
+  "images/football-img.png"
 ];
 
 const risegamelinks = [
+  "./pages/traffic-game.html",
+  "./pages/mathpuzzle.htmll",
+  "https://chrome-trex-game1.netlify.app/",
+  "./pages/roll-dice-game.html",
   "./pages/tic-tac-toe-game.html",
-  "./pages/tic-tac-toe-game.html",
-  "./pages/tic-tac-toe-game.html",
-  "./pages/tic-tac-toe-game.html",
-  "./pages/tic-tac-toe-game.html",
-  "./pages/tic-tac-toe-game.html"
+  "https://2-player-football.netlify.app/"
 ];
 
 const loadrisegames = () => {
@@ -63,20 +59,54 @@ loadrisegames();
 
 
 
-document.getElementById("top-btn").addEventListener("click", function() {
-    document.getElementById("top-games-container").scrollIntoView(
-        {
-            behavior:"smooth"
-        }
-    );
+const epicgameimgs = [
+  "images/emoji-catch.png",
+  "images/typings-game.webp",
+  "images/stone-paper.jpg",
+  "images/guess-the-number.jpg",
+  "images/hard-2d.jpg",
+  "images/fruit-ninja.jpg",
+];
+
+const epicgamelinks = [
+  "./pages/emoji-Catcher-game.html",
+  "./pages/typinggame.html",
+  "./pages/stone-paper-scissor.html",
+  "./pages/guess-the-number-game.html",
+  "https://hardest-2d-game.netlify.app/",
+  "https://fruit-ninja-game1.netlify.app/"
+
+];
+
+const loadepicgames = () => {
+  const gamesList = document.getElementById("epicgames");
+
+  for (let i = 0; i < epicgameimgs.length; i++) {
+    gamesList.innerHTML += `
+      <div class="game">
+        <a href="${epicgamelinks[i]}"><img src="${epicgameimgs[i]}" class="game-img"></a>
+      </div>`;
+  }
+};
+
+loadepicgames();
+
+
+
+document.getElementById("top-btn").addEventListener("click", function () {
+  document.getElementById("top-games-container").scrollIntoView(
+    {
+      behavior: "smooth"
+    }
+  );
 });
 
-document.getElementById("rise-btn").addEventListener("click", function() {
-    document.getElementById("rise-games-container").scrollIntoView(
-        {
-            behavior:"smooth"
-        }
-    );
+document.getElementById("rise-btn").addEventListener("click", function () {
+  document.getElementById("rise-games-container").scrollIntoView(
+    {
+      behavior: "smooth"
+    }
+  );
 });
 
 
@@ -86,23 +116,23 @@ document.getElementById('site-year').textContent = new Date().getFullYear();
 
 
 
-const pages= document.getElementById("pages");
+const pages = document.getElementById("pages");
 let isOpen = false;
 
 function showPages() {
-    pages.style.display="flex";
-    isOpen = true;
+  pages.style.display = "flex";
+  isOpen = true;
 }
 
 function hidepages() {
-        pages.style.display="none";
-    isOpen = false;
-} 
+  pages.style.display = "none";
+  isOpen = false;
+}
 
 function toggleHamburger() {
-    if(isOpen) {
-        hidepages();
-    } else {
-        showPages();
-    }
+  if (isOpen) {
+    hidepages();
+  } else {
+    showPages();
+  }
 }
